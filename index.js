c093ab649916dabf60998fbb4ec6536185f6d8b3
Andrey Volkov <[fullon1988@gmail.com]>
2020-10-01 09:42:24 +0300

    addAnimation

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 class Table {
   constructor(tableSelector) {
     this.table = document.querySelector(tableSelector);
-    this.bar = this.table.querySelector(".bar");
-    this.grid = this.table.querySelector(".table-grid");
+    this.bar = this.table.querySelector('.bar');
+    this.grid = this.table.querySelector('.table-grid');
     this.tableSelector = this.init() 
   }
 
   toggleActive(target) {
-    this.bar.querySelectorAll(".bar__item_active").forEach((item) => {
-      item.classList.remove("bar__item_active");
+    this.bar.querySelectorAll('.bar__item_active').forEach((item) => {
+      item.classList.remove('bar__item_active');
     });
 
     const btn = target;
-    btn.classList.add("bar__item_active");
+    btn.classList.add('bar__item_active');
   }
 
   setColor(color) {
@@ -22,74 +22,73 @@ class Table {
   toggle(event) {
     const target = event.target;
     const targetColor = target.dataset.color;
-    if (!target.classList.contains("bar__item")) return false;
+    if (!target.classList.contains('bar__item')) return false;
 
     this.toggleActive(target);
     this.setColor(targetColor);
   }
 
   init() {
-    this.bar.addEventListener("click", (event) => this.toggle(event));
+    this.bar.addEventListener('click', (event) => this.toggle(event));
   }
 }
 
-const myTable = new Table(".table");
+const myTable = new Table('.table');
 
-let btns = document.querySelectorAll(".table-grid__item");
+const btns = document.querySelectorAll('span');
 
-let input = document.getElementById("value");
+const input = document.getElementById('value');
 
 //Функция добавления чисел в инпут
-let clearBtn = document.querySelector(".header__button");
+let clearBtn = document.querySelector('.header__button');
 btns.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    console.log("number");
+  item.addEventListener('click', (e) => {
+    console.log('number');
     let num = +item.innerHTML;
     input.value = num;
   });
 });
 
 //Функция очистки инпут
-clearBtn.addEventListener("click", (e) => {
-  input.value = "";
+clearBtn.addEventListener('click', (e) => {
+  input.value = '';
 });
 
-//Функция рандомной смены элементов
-const cards = document.querySelectorAll(".table-grid__item");
+// Функция рандомной смены элементов
+const cardsBox = document.querySelectorAll('.table-grid')[0];
+const cards = document.querySelectorAll('.table-grid__item');
 
 function shuffle() {
-  console.log("randomCard");
+  console.log('randomCard');
   cards.forEach((card) => {
     let ramdomPos = Math.floor(Math.random() * cards.length);
     card.style.order = ramdomPos;
   });
 }
 
-setInterval(() => shuffle(), 5000);
+setInterval(() => {
+  shuffle();
+  cardsBox.classList.add('opacity');
+  setTimeout(() => cardsBox.classList.remove('opacity'), 333);
+}, 5000);
 
 //функция drag & drop
-const tasksListElement = document.querySelector(".table-grid");
-const taskElements = tasksListElement.querySelectorAll(".table-grid__item");
+const tasksListElement = document.querySelector('.table-grid');
+const taskElements = tasksListElement.querySelectorAll('.table-grid__item');
 
-// Перебираем все элементы списка и присваиваем нужное значение
-for (const task of taskElements) {
-  task.draggable = true;
-}
-
-tasksListElement.addEventListener("dragstart", (evt) => {
-  evt.target.classList.add("selected");
+tasksListElement.addEventListener('dragstart', (evt) => {
+  evt.target.classList.add('selected');
 });
 
-tasksListElement.addEventListener("dragend", (evt) => {
-  evt.target.classList.remove("selected");
+tasksListElement.addEventListener('dragend', (evt) => {
+  evt.target.classList.remove('selected');
 });
 
-tasksListElement.addEventListener("dragover", (evt) => {
+tasksListElement.addEventListener('dragover', (evt) => {
   // Разрешаем сбрасывать элементы в эту область
   evt.preventDefault();
-
   // Находим перемещаемый элемент
-  const activeElement = tasksListElement.querySelector(".selected");
+  const activeElement = tasksListElement.querySelector('.selected');
   // Находим элемент, над которым в данный момент находится курсор
   const currentElement = evt.target;
   // Проверяем, что событие сработало:
@@ -97,9 +96,9 @@ tasksListElement.addEventListener("dragover", (evt) => {
   // 2. именно на элементе списка
   const isMoveable =
     activeElement !== currentElement &&
-    currentElement.classList.contains("table-grid__item");
+    currentElement.classList.contains('.table-grid__item');
 
-  // Если нет, прерываем выполнение функции
+  // // Если нет, прерываем выполнение функции
   if (!isMoveable) {
     return;
   }
@@ -131,14 +130,14 @@ const getNextElement = (cursorPosition, currentElement) => {
   return nextElement;
 };
 
-tasksListElement.addEventListener("dragover", (evt) => {
+tasksListElement.addEventListener('dragover', (evt) => {
   evt.preventDefault();
 
-  const activeElement = tasksListElement.querySelector(".selected");
+  const activeElement = tasksListElement.querySelector('.selected');
   const currentElement = evt.target;
   const isMoveable =
     activeElement !== currentElement &&
-    currentElement.classList.contains("table-grid__item");
+    currentElement.classList.contains('table-grid__item');
 
   if (!isMoveable) {
     return;
@@ -153,7 +152,7 @@ tasksListElement.addEventListener("dragover", (evt) => {
     (nextElement && activeElement === nextElement.previousElementSibling) ||
     activeElement === nextElement
   ) {
-    // Если нет, выходим из функции, чтобы избежать лишних изменений в DOM
+  //   // Если нет, выходим из функции, чтобы избежать лишних изменений в DOM
     return;
   }
 
